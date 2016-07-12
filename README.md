eb2288310b7b35a3bff651d502aa5cac3b34fa02
kongzhidea <563150913@qq.com>
2016-07-12 11:45:14 +0800

    add java.image, kaptcha

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # captcha
 图片验证码服务
+
+推荐 captcha
---
+++ kaptcha/KatpchaServlet.java
@@ -0,0 +1,73 @@
+package captchera;
+
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.util.Config;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+import javax.imageio.ImageIO;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class KatpchaServlet extends HttpServlet implements Servlet {
+	private Properties props = new Properties();
+
+	private Producer kaptchaProducer = null;
+
+	private String sessionKeyValue = null;
+
+	public void init(ServletConfig conf) throws ServletException {
+		super.init(conf);
+
+		ImageIO.setUseCache(false);
+
+		Enumeration initParams = conf.getInitParameterNames();
+		while (initParams.hasMoreElements()) {
+			String key = (String) initParams.nextElement();
+			String value = conf.getInitParameter(key);
+			this.props.put(key, value);
+		}
+
+		Config config = new Config(this.props);
+		this.kaptchaProducer = config.getProducerImpl();
+		this.sessionKeyValue = config.getSessionKey();//kaptcha.session.key
+		
+	}
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.setDateHeader("Expires", 0L);
+
+		resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+
+		resp.addHeader("Cache-Control", "post-check=0, pre-check=0");
+
+		resp.setHeader("Pragma", "no-cache");
+
+		resp.setContentType("image/jpeg");
+
+		String capText = this.kaptchaProducer.createText();
+
+//		req.getSession().setAttribute(this.sessionKeyValue, capText);
+		
+		System.out.println(capText);
+
+		BufferedImage bi = this.kaptchaProducer.createImage(capText);
+
+		ServletOutputStream out = resp.getOutputStream();
+
+		ImageIO.write(bi, "jpg", out);
+		try {
+			out.flush();
+		} finally {
+			out.close();
+		}
+	}
+}
